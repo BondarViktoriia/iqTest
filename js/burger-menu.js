854ca10dcf647c78a9d2612b1566347f8e71fd2f
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-18 16:55:53 +0300

    add start file+burger menu+hero section

---
+++ js/burger-menu.js
@@ -0,0 +1,31 @@
+// Получаем элементы кнопки бургер-меню и меню
+const burgerMenu = document.querySelector(".burger-menu");
+const menu = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close-button")
+
+// Обработчик события клика на кнопке бургер-меню
+burgerMenu.addEventListener("click", function() {
+  // Переключаем класс, который показывает или скрывает меню
+  menu.classList.toggle("show-menu");
+    // Получаем текущее значение свойства display для меню
+  const menuDisplay = menu.style.display;
+
+  // Если меню скрыто, показываем его
+  if (menuDisplay === "none") {
+    menu.style.display = "block";
+  }
+  // Если меню отображается, скрываем его
+  else {
+    menu.style.display = "none";
+  
+  }
+  
+  // Переключаем классы для полосок в кнопке бургер-меню
+    burgerMenu.classList.toggle("open");
+});
+
+closeBtn.addEventListener('click', function () {
+  menu.style.display = 'none';
+  burgerMenu.classList.toggle("open");
+
+})
