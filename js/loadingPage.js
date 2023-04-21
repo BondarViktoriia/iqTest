66f9d7124214d9c88f5cfbd09ac63d7d0fafeb59
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-21 15:54:46 +0300

    add loading section

--- js/loadingPage.js
+++ js/loadingPage.js
@@ -1,6 +1,10 @@
 const buttonLoading = document.querySelector('.btn-loading');
 const loadingSection = document.querySelector('.section-loading');
+const starSection = document.querySelector('.star-section');
+const progress = document.querySelector('.progress');
 
 buttonLoading.addEventListener('click', () => {
+    starSection.style.display = 'none';
     loadingSection.style.display = "block";
+    progress.style.width = '100%';
 })
