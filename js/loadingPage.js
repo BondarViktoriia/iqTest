27bbbd2202c4fd123e0da9f7433fc009d28f8b1d
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-21 17:07:41 +0300

    add done page

--- js/loadingPage.js
+++ js/loadingPage.js
@@ -1,10 +1,20 @@
-const buttonLoading = document.querySelector('.btn-loading');
-const loadingSection = document.querySelector('.section-loading');
-const starSection = document.querySelector('.star-section');
-const progress = document.querySelector('.progress');
+const buttonLoading = document.querySelector(".btn-loading");
+const loadingSection = document.querySelector(".section-loading");
+const starSection = document.querySelector(".star-section");
+const progress = document.querySelector(".progress");
+const done = document.querySelector(".done");
+const test = document.querySelector('.burger-container')
+const testDone=document.querySelector('.test-done')
 
-buttonLoading.addEventListener('click', () => {
-    starSection.style.display = 'none';
-    loadingSection.style.display = "block";
-    progress.style.width = '100%';
-})
+buttonLoading.addEventListener("click", () => {
+  starSection.style.display = "none";
+  loadingSection.style.display = "block";
+  progress.style.width = "100%";
+  setTimeout(function () {
+    loadingSection.style.display = "none";
+    done.style.display = "block";
+      progress.style.display = "none";
+      test.style.display = 'none';
+      testDone.style.display = 'block';
+  }, 2000);
+});
