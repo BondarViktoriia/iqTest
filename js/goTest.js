53d6b2a75a7c345d39e173f524a4c8cdf3a3879b
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-25 17:01:39 +0300

    combine two files

---
+++ js/goTest.js
@@ -0,0 +1,10 @@
+
+const goBtn = document.querySelectorAll('.btn-test');
+const test = document.querySelector('.testIQ');
+const startPage =document.querySelector('.startPage')
+
+goBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        startPage.style.display = 'none';
+    test.style.display = 'block';})
+})
--- js/nextPage.js
+++ js/nextPage.js
@@ -3,7 +3,6 @@ const sections = document.querySelectorAll('.section-question');
 const progress = document.querySelector('.progress');
 let currentQuestion = 0;
 
-// функция для скрытия текущего вопроса и отображения следующего
 function showNextQuestion() {
   sections[currentQuestion].style.display = 'none';
   currentQuestion++;
@@ -11,13 +10,11 @@ function showNextQuestion() {
   updateProgressBar();
 }
 
-// функция для обновления прогресс-бара
 function updateProgressBar() {
   const percent = (currentQuestion / sections.length) * 100;
   progress.style.width = percent + '%';
 }
 
-// добавляем обработчик событий на кнопку "Далее"
 btnNext.forEach((btn) => {
   btn.addEventListener('click', showNextQuestion);
 });
